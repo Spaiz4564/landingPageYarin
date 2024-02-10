d247192597d75ac6b4834326f76461b2c91982da
Guy <guyshilon30@gmail.com>
2024-02-10 18:12:57 +0200

    Add files via upload

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -4,19 +4,22 @@
       <div class="image"></div>
       <div class="text">
         <h1>לייעוץ מקצועי מלאו פרטים בטופס</h1>
-        <p>אני מאמין ביחס רפואי אישי, המעניק תחושה של ביטחון ולהקנות לכם לאכול ולחייך בביטחון, הגיעו לבדיקת ייעוץ ללא עלות.</p>
-        <form class="form"  action="https://formspree.io/f/xdoqenbl" method="POST">
+        <p>אני מאמין ביחס רפואי אישי המעניק תחושת ביטחון למטופל, המטרה שלי היא לאפשר לכם לאכול ולחייך בחזרה.</p>
+        <form class="form" @submit.prevent="submitForm" method="POST">
           <div class="inputs">
-             <div class="form_input">
-            <label class="label" for="full-name">שם מלא</label>
-            <input class="input" id="full-name" type="text" name="full-name" required="">
+            <div class="form_input">
+              <label class="label" for="full-name">שם מלא</label>
+              <input v-model="formData.fullName" class="input" id="full-name" type="text" name="full-name" required>
+            </div>
+            <div class="form_input">
+              <label class="label" for="number">מס' טלפון</label>
+              <input v-model="formData.number" class="input" id="number" type="text" name="number" required>
+            </div>
           </div>
-          <div class="form_input">
-            <label class="label" for="number">מס' טלפון</label>
-            <input class="input" id="number" type="text" name="number" required="">
-          </div>
-          </div>
-          <button class="submit_form">חזרו אליי</button>
+          <button :disabled="loading" type="submit" class="submit_form">
+            <span :class="['btn_text' ,{'loading': loading}]" >{{ btnText }}</span>
+            <span v-if="loading" class="loading-animation"></span>
+          </button>
         </form>
       </div>
     </div>
@@ -25,14 +28,77 @@
 
 <script>
 import { createScrollRevealDirective } from 'vue-scroll-reveal';
+import axios from 'axios';
+
 export default {
   name: 'ContactSection',
   directives: {
     'scroll-reveal': createScrollRevealDirective(),
   },
-}
+  data() {
+    return {
+      loading: false,
+      submitted: false,
+      formData: {
+        fullName: '',
+        number: ''
+      }
+    };
+  },
+  computed: {
+    btnText() {
+      return this.submitted ? 'פנייתך התקבלה בהצלחה' : 'חזרו אליי';
+    }
+  },
+  methods: {
+    submitForm() {
+      this.loading = true;
+      axios.post('https://formspree.io/f/xdoqenbl', this.formData)
+        .then(response => {
+          this.submitted = true;
+          console.log('Form submitted successfully:', response.data);
+        })
+        .catch(error => {
+          console.error('Error submitting form:', error);
+        })
+        .finally(() => {
+          setTimeout(() => {
+            this.loading = false;
+            this.resetInputs();
+          }, 1100);
+        });
+    },
+
+    resetInputs() {
+      this.formData = {
+        fullName: '',
+        number: ''
+      };
+    }
+  }
+};
 </script>
 
 <style>
+ .submit_form {
+    position: relative;
+    overflow: hidden;
+  }
 
+  .loading-animation {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 0;
+    height: 100%;
+    background-color: #805a49ea;
+    animation: loading 2s ease-out;
+    padding: 1.2rem;
+  }
+
+  @keyframes loading {
+    to {
+      width: 100%;
+    }
+  }
 </style>
