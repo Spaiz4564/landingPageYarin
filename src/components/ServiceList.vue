bf243c01a208898e3e93687198ab6b5c53ad6254
Guy <guyshilon30@gmail.com>
2024-02-14 18:19:58 +0200

    fix

--- src/components/ServiceList.vue
+++ src/components/ServiceList.vue
@@ -1,7 +1,8 @@
 <template>
 <ul class="services">
  <li class="service" v-for="(service,i) in services" :key="i" :style="{backgroundColor: service.primaryColor}">
-<img class="check" src="../assets/img/check.png" alt="">
+  <img class="check" src="../assets/img/check.png" alt="">
+  <h1 class="service_title">{{ service.title }}</h1>
  <p class="service_text">{{ service.text }}</p>
  </li>
 </ul>
